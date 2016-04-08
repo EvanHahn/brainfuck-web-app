1d371d268e584331ae2f3a49c38da747a6df473f
Evan Hahn <me@evanhahn.com>
2016-04-08 10:27:47 -0700

    add a stupid bf printer thing

---
+++ utils/bf-print.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from sys import argv
+
+value = 0
+
+for c in argv[1]:
+    desired_value = ord(c)
+    diff = abs(desired_value - value)
+    if desired_value > value:
+        increments = '+' * diff
+    else:
+        increments = '-' * diff
+
+    print(c + ' ' + increments + '.')
+
+    value = desired_value
